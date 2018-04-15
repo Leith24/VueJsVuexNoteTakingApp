52bd756d1ebe6068077e08fb057896543f7e7bf3
Leith24 <gavindleith@gmail.com>
2018-04-15 12:43:52 -0500

    finalized app with vuex tied to all components and html

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ const mutations = {
     let newNote = payload;
     state.notes.push(newNote);
   },
-  ADD_TIMESTAMP() {
+  ADD_TIMESTAMP(state, payload) {
     let newTimeStamp = payload;
     state.timestamps.push(newTimeStamp);
   }
@@ -37,13 +37,47 @@ const store = new Vuex.Store({
 })
 
 const inputComponent = {
-  template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
+  template: `<input placeholder='Enter a note' 
+      v-model="input"
+      @keyup.enter="monitorEnterKey"
+      class="input is-small" type="text" />`,
+  data(){
+    return {
+      input: '',
+    }
+  },
+  methods:{
+    monitorEnterKey() {
+      this.$store.dispatch('addNote', this.input);
+      this.$store.dispatch('addTimestamp', new Date().toLocaleString());
+      this.input = '';
+    }
+  }
+}
+const noteCountComponent = {
+  template: `<div class="note-count">
+  Note count: <strong> {{ noteCount }} </strong>
+  </div>`,
+  computed: {
+    noteCount(){
+      return this.$store.getters.getNoteCount;
+    }
+  }
 }
 
 new Vue({
   el: '#app',
   store,
+  computed:{
+    notes(){
+      return this.$store.getters.getNotes;
+    },
+    timestamps(){
+      return this.$store.getters.getTimestamps;
+    }
+  },
   components: {
-    'input-component': inputComponent
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent
   }
 })
