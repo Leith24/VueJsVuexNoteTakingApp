274a535b6e15c90aa88590a5b3021a8f6893d514
Leith24 <gavindleith@gmail.com>
2018-04-15 12:26:56 -0500

    added state, mutations, actions, and getters

--- app/main.js
+++ app/main.js
@@ -1,3 +1,34 @@
+const state = {
+  notes: [],
+  timestamps: []
+}
+
+const mutations = {
+  ADD_NOTE(state, payload) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP() {
+    let newTimeStamp = payload;
+    state.timestamps.push(newTimeStamp);
+  }
+}
+
+const actions = {
+  addNote(context, payload){
+    context.commit('ADD_NOTE', payload);
+  },
+  addTimestamp(context, payload){
+    context.commit('ADD_TIMESTAMP', payload);
+  }
+}
+
+const getters = {
+  getNotes: state => state.notes,
+  getTimestamps: state => state.timestamps,
+  getNoteCount: state => state.notes.length,
+}
+
 const inputComponent = {
   template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
 }
